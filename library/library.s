09291aa5e6baff63ac37fc9423f3f2bc3e46cd8b
Keiichi Watanabe <udon.watanabe@gmail.com>
2014-11-18 18:42:08 +0900

    fix floor

--- library/library.s
+++ library/library.s
@@ -2,8 +2,6 @@ min_caml_print:
         rsb     $v0
         jr      $ra
 
-
-
 # floor(x)
 min_caml_floor:
         mtc1    $zero, $f1                # $f1 <-  0.0
@@ -14,11 +12,11 @@ min_caml_floor:
         li      $s0, 0xcb000000           #
         mtc1    $s0, $f5                  # $f5 <- -8388608.0
         c.eq.s  $f0, $f1                  # if (isDenormal($f0))
-        bc1t    floor.1                   #   goto floor.2
+        bc1t    floor.2                   #   goto floor.2
         c.olt.s $f0, $f1                  # if ($f0 < $f1) //$f1 == 0
-        bc1t    floor.3                   #   goto floor.1
+        bc1t    floor.1                   #   goto floor.1
         c.olt.s $f3, $f0                  # if (8388608.0 < $f0)
-        bc1t    floor.2                   #   goto floor.3
+        bc1t    floor.3                   #   goto floor.3
         add.s   $f2, $f0, $f3             # $f2 <- $f0 + 8388608.0
         sub.s   $f2, $f2, $f3             # $f2 <- $f2 - 8388608.0
         c.ole.s $f2, $f0                  # if ($f2 <= $f0) //OK
@@ -29,7 +27,7 @@ min_caml_floor:
 
 floor.1:                                  # // $f0 < 0.0
         c.olt.s $f0, $f5                  # if ($f0 < -8388608.0)
-        bc1t    floor.2                   #   goto floor.3
+        bc1t    floor.3                   #   goto floor.3
         sub.s   $f2, $f0, $f3             # $f2 <- $f0 - 8388608.0
         add.s   $f2, $f2, $f3             # $f2 <- $f2 + 8388608.0
         c.ole.s $f2, $f0                  # if ($f2 <= $f0)
@@ -37,9 +35,9 @@ floor.1:                                  # // $f0 < 0.0
         sub.s   $f0, $f2, $f4             # $f0 <- $f2 - 1.0
         jr      $ra                       # return $f0
 
-floor.2:
+floor.2:                                  # // Denormal
         mov.s   $f0, $f1                  # $f0 <- $f1 //$f1 == 0
-floor.3:
+floor.3:                                  # // return
         jr      $ra                       # return $f0
 floor.4:
         mov.s   $f0, $f2                  # $f0 <- $f2
