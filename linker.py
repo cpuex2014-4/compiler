0fd283722ebcd55ee943cc7a15e7ceeaa2815fff
prologician <t.yamashita1238@gmail.com>
2014-12-12 16:24:42 +0900

    get able to link multi-libraries

--- linker.py
+++ linker.py
@@ -8,23 +8,13 @@ def add_init(out):
     out.write('\tj\tmain\n')
     return
 
-def concat(lib, f, out):
-    lib_data = lib.read()
-    file_data = f.read()
-    out.write(lib_data)
-    out.write(file_data)
-    return
-
 if __name__ == '__main__':
     file_name = sys.argv[1]
     out_name = sys.argv[2]
-    lib_name = '../kake/library/arrayLib.s'
-    flib_name = '../kake/library/floatLib.s'
     with open(out_name,'w') as out:
         add_init(out)
-        with open(lib_name,'r') as lib:
-            out.write(lib.read())
-        with open(flib_name, 'r') as flib:
-            out.write(flib.read())
-        with open(file_name,'r') as f:
+        for i in sys.argv[3:]:
+            with open(i,'r') as lib:
+                out.write(lib.read())
+        with open(file_name, 'r') as f:
             out.write(f.read())
